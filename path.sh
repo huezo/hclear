d12802a01e1800b6b6e94863a6bf1e44abb73650
HUEZO <huezo@users.noreply.github.com>
2017-05-05 22:25:54 -0600

    Automatizado
    Que el script se ejecute en cualquier lugar de la Terminal:

---
+++ path.sh
@@ -0,0 +1,15 @@
+##!/bin/bash
+
+wget https://raw.githubusercontent.com/huezo/hclear/master/hclear.sh
+
+sudo chmod +x hclear.sh
+
+sudo mv hclear.sh /usr/bin/hclear.sh
+
+sudo mv /usr/bin/hclear.sh /usr/bin/hclear
+
+
+
+echo "fin"
+
+
