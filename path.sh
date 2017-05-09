23f5286aaae1b9e0621bf2893f1b7e63264270ca
HUEZO <huezo@users.noreply.github.com>
2017-05-09 15:28:57 -0600

    automatizado
    v 3.1

--- path.sh
+++ path.sh
@@ -1,15 +1,27 @@
 ##!/bin/bash
 
-wget https://raw.githubusercontent.com/huezo/hclear/master/hclear.sh
+# 
+# wget -N https://raw.githubusercontent.com/huezo/hclear/master/hclear.sh
+
+
+wget -N https://raw.githubusercontent.com/huezo/hclear/master/hclear.sh
+
+#
 
 sudo chmod +x hclear.sh
 
+#
+
 sudo mv hclear.sh /usr/bin/hclear.sh
+
+#
 
 sudo mv /usr/bin/hclear.sh /usr/bin/hclear
 
+#
 
+echo "ahora solo escribe en Terminal:"
+
+echo "hclear"
 
 echo "fin"
-
-
