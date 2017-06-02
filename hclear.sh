b984b747a5015400675e6788b9665731e78b78c5
HUEZO <huezo@users.noreply.github.com>
2017-06-02 10:50:25 -0600

    v 3.0
    ===========================================================
      
    0 = Instalacion de paquetes 
    1 = LIMPIAR: autoclean , clean y autoremove 
    2 = ACTUALIZAR: update , upgrade y dist-upgrade 
    3 = elimina repositorio PPA y revierte cambios: ppa-purge  
    4 = desinstala el programa y eliminar todos los archivos de configuracion
    5 = desinstala programa y mantener los archivos de configuración
    6 =  Reinstalar paquetes o Programa 
    7 = Buscar apquetes con apt-cache search
    8 = Limpieza extrema-Kernel
      
    ===========================================================

--- hclear.sh
+++ hclear.sh
@@ -8,6 +8,9 @@
 #
 #########
 
+
+
+
 echo '¿Qué quieres hacer?'
 
 echo "==========================================================="
@@ -25,14 +28,29 @@ echo "4 = desinstala el programa y eliminar todos los archivos de configuracion"
 
 echo "5 = desinstala programa y mantener los archivos de configuración"
 
+
+echo "6 =  Reinstalar paquetes o Programa "
+
+echo "7 = Buscar apquetes con apt-cache search"
+
+echo "8 = Limpieza extrema-Kernel"
+
 echo "  "
 echo "==========================================================="
 echo "  "
 
-read A
 
-if [ $A = 0 ];
-then
+
+
+
+read A
+  
+
+
+case $A in
+     0)
+        
+
 
 echo "##########"
 echo "# para instalar 2" 
@@ -55,16 +73,19 @@ sudo sudo apt-get install $huezo
 
 
 
-exit 0
-
-#elif [ $A = 1 ];
-#then
 
 
-#exit 0
 
-elif [ $A = 1 ];
-then
+
+
+
+
+     ;;
+
+
+     1)
+      
+
 #
 #Elimina del cache los paquetes .deb con versiones anteriores a 
 #los de los programas que tienes instalados.
@@ -88,11 +109,18 @@ sudo apt-get clean
 
 sudo apt-get autoremove
 
-exit 0
 
 
-elif [ $A = 2 ];
-then
+
+
+
+
+
+     ;;
+
+
+     2)
+       
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -103,10 +131,13 @@ sudo apt-get dist-upgrade
 #sudo apt upgrade
 #sudo apt dist-upgrade
 
-exit 0 
 
-elif [ $A = 3 ]; 
-then
+
+     ;;
+
+
+     3)
+     
 
 echo 'digita la ppa de esta manera ejemplo:'
 echo "ppa:ubuntu-mozilla-security/ppa"
@@ -116,10 +147,15 @@ read ppa
 sudo ppa-purge $ppa
 
 
-exit 0 
 
-elif [ $A = 4 ];
 
+
+     ;;
+
+
+     4)
+
+      
 
 #echo "4 = desinstala el programa y eliminar todos los archivos de configuracion"
 
@@ -143,12 +179,20 @@ read programa
 
 sudo apt-get --purge remove $programa
 
-then
-
-exit 0 
 
 
-elif [ $A = 5 ];
+
+
+
+
+
+
+     ;;
+
+
+     5)
+        
+
 
 echo ""
 echo "##########"
@@ -176,14 +220,132 @@ sudo apt-get remove $programa1
 
 
 
-then
+
+
+
+
+
+
+
+
+     ;;
+
+
+     6)
+       
+
+#echo ""
+#echo "##########"
+#echo "# para Reinstalar 2" 
+#echo "#  o mas paquetes solo"
+#echo "# los escribes" 
+#echo "# separados por espacio"
+#echo "#"
+#echo "#########"
+#echo ""
+
+echo ""
+echo 'digita el paquete a reinstalar :'
+echo ""
+
+read programa1
+
+
+
+
+
+
+sudo apt-get install --reinstall $programa1
+
+
+
+     ;;
+
+     7)
+      
+echo ""
+echo 'digita el paquete a buscar :'
+echo ""
+
+read pro
+
+sudo apt-cache search $pro | more
 
 exit 0
 
-fi
+
+     ;;
 
 
-exit
+     8)
+        
+
+#deja los 3 ultimos Kernel
+# y kernel extra
+echo "deja los 3 ultimos Kernel y el Kernel extra"
+
+echo "¿ Desea continuar ? s/S para SI , n/N para No "
+
+ read opcion
+ 
+
+ case $opcion in
+     
+
+     s|S) 
+        
+
+
+ dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+
+
+
+      ;;
+
+
+      n|N)
+
+         echo "Cancelado"
+
+      ;;
+   
+
+  esac
+
+
+
+
+     ;;
+
+
+     *)
+        echo "no se qué numero es"
+     ;;
+
+
+
+
+     # *)
+     #   echo "no se qué numero es"
+     # ;;
+
+
+
+    # *)
+    #    echo "no se qué numero es"
+    # ;;
+
+
+
+
+  esac
+
+
+
+
+
+
+##!/bin/bash
 
 ##########
 #
@@ -192,4 +354,5 @@ exit
 #
 #
 #########
+
 
